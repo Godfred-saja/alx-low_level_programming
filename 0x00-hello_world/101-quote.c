34e81f196cd59d8aff60a0d771ff6b4a070423a6
Godfred-saja <godfredakowuah2002@gmail.com>
2023-03-16 19:06:43 -0700

    hans 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 * main - Entry point
 *
-* Return: Always 1 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+fputs("and thats piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (0);
 }
