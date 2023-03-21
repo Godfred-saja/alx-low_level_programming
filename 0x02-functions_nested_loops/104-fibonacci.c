59fb92177ed1fa7431fa9f561a6a97a9908cd8f3
Godfred-saja <godfredakowuah2002@gmail.com>
2023-03-21 16:21:04 -0700

    dgfxgfxgfx 7

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,6 @@
 * main - program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line
 *
 * Purpose - learn to code
-*
 * Return:  (Success)
 */
 int main(void)
