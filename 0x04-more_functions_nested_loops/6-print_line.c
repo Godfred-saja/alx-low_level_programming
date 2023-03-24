2ef0cc696b48eb48c6e7e69498e4f18af6d34b07
Godfred-saja <godfredakowuah2002@gmail.com>
2023-03-23 17:32:58 -0700

    gas 423

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_line - draws a straight line in the terminal
 * @n: number of times the character _ should be printed
@@ -13,7 +12,6 @@ _putchar('\n');
 else
 {
 int i;
-
 for (i = 1; i <= n; i++)
 {
 _putchar(' ');
