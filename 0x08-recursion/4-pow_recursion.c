bd127f88b98353bbd8519b096a562f7533762f21
Godfred-saja <godfredakowuah2002@gmail.com>
2023-04-05 14:51:44 -0700

    try 0

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _pow_recursion - function that returns the value of x raised to the power of y.
+* _pow_recursion - Function that returns the value of x raised to the power ofg
 * @x: input the function
 * @y: input the function
 * Return: 0
