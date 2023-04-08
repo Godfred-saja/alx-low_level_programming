b4bbc5020ce94b1a25ea9b9f9a8bc03ea114ce87
Godfred-saja <godfredakowuah2002@gmail.com>
2023-04-08 08:11:09 -0700

    try 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - program that prints its name, followed by a new line.
+* @argc: argument counter
+* @argv: argument value
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[]);
+{
+printf("%s\n", argv[argc - argc]);
+return (0);
+}
