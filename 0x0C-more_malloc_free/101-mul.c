933daadd2b2d21178ead2ae6322f7c9a7b8f8a71
Godfred-saja <godfredakowuah2002@gmail.com>
2023-04-13 07:33:10 -0700

    asZD

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,14 +6,14 @@
 /**
 * main - multiplies two positive numbers
 * @argc: n arguments function
-* @argv: args function
+* @argv: args fuction
 * Return: int
 **/
 
 int main(int argc, char *argv[])
 {
 unsigned long mul;
-int i, p;
+int i, j;
 
 if (argc != 3)
 {
@@ -23,9 +23,9 @@ exit(98);
 
 for (i = 1; i < argc; i++)
 {
-for (p = 0; argv[i][p] != '\0'; p++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][p] > 57 || argv[i][p] < 48)
+if (argv[i][j] > 57 || argv[i][j] < 48)
 {
 printf("Error\n");
 exit(98);
