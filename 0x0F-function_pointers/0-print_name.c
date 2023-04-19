37d5c4d19c078d01d409b985d58f3875f5961ddb
Godfred-saja <godfredakowuah2002@gmail.com>
2023-04-19 12:45:56 -0700

    agags

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
-* print_name - function that print a name
+* print_name - prints a name
 * @name: name to print
-* @f: printing function
+* @f: pointer to the printing function
 */
 void print_name(char *name, void (*f)(char *))
 {
