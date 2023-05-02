d11dd098aee676f5ed97aa41205682e52a43b07d
Godfred-saja <godfredakowuah2002@gmail.com>
2023-05-02 09:08:46 -0700

    lams

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,17 +4,17 @@
 * print_listint - Prints all elements of a list
 * @h: Pointer to a list
 *
-* Return: The number of nodes in the list_t list
+* Return: The number of codes in the list_t list
 */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
+size_t codes = 0;
 
 while (h)
 {
-nodes++;
+codes++;
 printf("%d\n", h->n);
 h = h->next;
 }
-return (nodes);
+return (codes);
 }
