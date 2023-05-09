89f2a02408cfd8f39b3d383e79ab9d413d1d0397
Godfred-saja <godfredakowuah2002@gmail.com>
2023-05-08 18:12:58 -0700

    lans

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -273,10 +273,10 @@ exit(98);
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 Elf64_Ehdr *header;
-int o, r;
+int n, r;
 
-o = open(argv[1], O_RDONLY);
-if (o == -1)
+n = open(argv[1], O_RDONLY);
+if (n == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
@@ -284,15 +284,15 @@ exit(98);
 header = malloc(sizeof(Elf64_Ehdr));
 if (header == NULL)
 {
-close_elf(o);
+close_elf(n);
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
-r = read(o, header, sizeof(Elf64_Ehdr));
+r = read(n, header, sizeof(Elf64_Ehdr));
 if (r == -1)
 {
 free(header);
-close_elf(o);
+close_elf(n);
 dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 exit(98);
 }
@@ -309,6 +309,6 @@ print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
 
 free(header);
-close_elf(o);
+close_elf(n);
 return (0);
 }
